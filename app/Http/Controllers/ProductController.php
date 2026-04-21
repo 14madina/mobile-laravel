f0f7a2245bcf93fca9cda0eba6a3df82c42b02e0
Siti Madina Halim <smadinahalim@gmail.com>
2026-04-21 21:19:02 +0700

    Initial commit Flutter and Laravel project

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $prods = Product::get();
+
+        if (request()->segment(1) == 'api') {
+            return response()->json([
+                'error' => false,
+                'list'  => $prods,
+            ]);
+        }
+
+        return view('view_product', [
+            'title' => 'Daftar Produk',
+            'data'  => $prods,
+        ]);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
